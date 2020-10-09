5572bf9c5054559d4f741ebf08b6f851dfad139a
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-10-09 02:39:28 -0700

    Messages render diferent color if they are users or other users

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
+
 export default function Message(props) {
+
+    console.log(`username is ${props.username}`)
+    console.log(`message username is ${props.message.username}`)
+    // Checks if username is same as message user
+    const isUser= props.username === props.message.username
     return (
-        <div>
-            <h2>{props.username}</h2>
-            <p>{props.message}</p>
+        <div className={(isUser ? 'user_message' : 'not_user_message')}>
+            <h2>{props.message.username}</h2>
+            <p>{props.message.message}</p>
         </div>
     )
 }
